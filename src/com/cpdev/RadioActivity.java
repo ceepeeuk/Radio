a3b8e7487c3a385da1a7f1d994f593ef7deac758
Chris Peters <chris.peters.uk@gmail.com>
2011-04-05 13:38:59 +0100

    Basic async playing rinse url

--- src/com/cpdev/RadioActivity.java
+++ src/com/cpdev/RadioActivity.java
@@ -1,19 +1,20 @@
 package com.cpdev;
 
 import android.app.Activity;
-import android.media.MediaPlayer;
+import android.media.AsyncPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.io.IOException;
+import static android.media.AudioManager.STREAM_MUSIC;
 
 public class RadioActivity extends Activity {
 
     Button btnPlay;
     Button btnRecord;
-    MediaPlayer mediaPlayer;
+    AsyncPlayer mediaPlayer;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -32,25 +33,14 @@ public class RadioActivity extends Activity {
 
     public void playClick(View playButton) {
         setStatus("Starting..");
-        mediaPlayer = new MediaPlayer();
-
-        try {
-            mediaPlayer.setDataSource("http://podcast.dgen.net:8000/rinseradio");
-            setStatus("Buffering...");
-            mediaPlayer.prepare();
-            mediaPlayer.start();
-        } catch (IOException e) {
-            setStatus("Oops error occurred...");
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
-
+        mediaPlayer = new AsyncPlayer("");
+        mediaPlayer.play(this, Uri.parse("http://podcast.dgen.net:8000/rinseradio"), false, STREAM_MUSIC);
         setStatus("Playing...");
     }
 
     public void stopClick(View playButton) {
         if (mediaPlayer != null) {
             mediaPlayer.stop();
-            mediaPlayer.release();
         }
 
         setStatus("Stopped...");
@@ -59,4 +49,6 @@ public class RadioActivity extends Activity {
     public void recordClick(View recordButton) {
         setStatus("Recording...");
     }
+
+
 }
