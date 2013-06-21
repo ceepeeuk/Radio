43ca88081e90a6b00767ef9af063ddacddc45f5c
Chris Peters <chris.peters.uk@gmail.com>
2013-06-21 23:20:30 +0100

    Working tab framework

--- src/com/statichiss/recordio/MainActivity.java
+++ src/com/statichiss/recordio/MainActivity.java
@@ -1,12 +1,18 @@
 package com.statichiss.recordio;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTabHost;
+
+import com.statichiss.R;
+import com.statichiss.recordio.fragments.PlayerFragment;
+import com.statichiss.recordio.fragments.RecordingsFragment;
+import com.statichiss.recordio.fragments.ScheduleFragment;
 
 /**
  * Created by chris on 20/06/2013.
  */
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
     // Fragment TabHost as mTabHost
     private FragmentTabHost mTabHost;
 
@@ -17,22 +23,21 @@ public class MainActivity extends Activity {
 
         mTabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
         mTabHost.setup(this, getSupportFragmentManager(), R.id.realtabcontent);
-        mTabHost.addTab(mTabHost.newTabSpec("battery").setIndicator("Battery",
-                getResources().getDrawable(R.drawable.ic_battery_tab)),
-                BatteryFragment.class, null);
-        mTabHost.addTab(mTabHost.newTabSpec("network").setIndicator("Network",
-                getResources().getDrawable(R.drawable.ic_network_tab)),
-                NetworkFragment.class, null);
-        mTabHost.addTab(mTabHost.newTabSpec("device").setIndicator("Device",
-                getResources().getDrawable(R.drawable.ic_device_tab)),
-                DeviceFragment.class, null);
+
+//        mTabHost.addTab(mTabHost.newTabSpec("battery").setIndicator("Battery",
+//                getResources().getDrawable(R.drawable.ic_battery_tab)),
+//                BatteryFragment.class, null);
+
+        mTabHost.addTab(mTabHost.newTabSpec("player").setIndicator("Player"), PlayerFragment.class, null);
+        mTabHost.addTab(mTabHost.newTabSpec("schedule").setIndicator("Schedule"), ScheduleFragment.class, null);
+        mTabHost.addTab(mTabHost.newTabSpec("recordings").setIndicator("Recordings"), RecordingsFragment.class, null);
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.main, menu);
+//        return true;
+//    }
 }
