2d4113143d871acf1cb3b5a259751d1cc7928813
Chris Peters <chris@air>
2013-07-03 23:32:12 +0100

    Fixed notification warnings

--- src/com/statichiss/recordio/NotificationHelper.java
+++ src/com/statichiss/recordio/NotificationHelper.java
@@ -14,25 +14,32 @@ public abstract class NotificationHelper {
     public static final int NOTIFICATION_RECORDING_ID = 2;
 
     public static Notification getNotification(Context context, int notificationId, RadioDetails radioDetails, CharSequence tickerText, CharSequence contentText, int flag) {
-        long when = System.currentTimeMillis();
         Notification notification;
 
         switch (notificationId) {
             case NOTIFICATION_PLAYING_ID:
-                notification = new Notification(R.drawable.ic_notification_playing, tickerText, when);
+                notification = new Notification.Builder(context)
+                        .setContentTitle(context.getString(R.string.app_name))
+                        .setContentText(contentText)
+                        .setTicker(tickerText)
+                        .setSmallIcon(R.drawable.ic_notification_playing)
+//                        .setLargeIcon(aBitmap)
+                        .build();
                 break;
             case NOTIFICATION_RECORDING_ID:
-                notification = new Notification(R.drawable.ic_notification_recording, tickerText, when);
+                notification = new Notification.Builder(context)
+                        .setContentTitle(context.getString(R.string.app_name))
+                        .setContentText(contentText)
+                        .setTicker(tickerText)
+                        .setSmallIcon(R.drawable.ic_notification_recording)
+//                        .setLargeIcon(aBitmap)
+                        .build();
                 break;
             default:
                 notification = new Notification();
         }
 
-        CharSequence contentTitle = context.getString(R.string.app_name);
-        Intent notificationIntent = new Intent(context, MainActivity.class);
-        PendingIntent contentIntent = PendingIntent.getActivity(context, 0, notificationIntent, 0);
-
-        notification.setLatestEventInfo(context, contentTitle, contentText, contentIntent);
+        notification.contentIntent = PendingIntent.getActivity(context, 0, new Intent(context, MainActivity.class), 0);
         notification.flags = flag;
 
         return notification;
@@ -41,29 +48,34 @@ public abstract class NotificationHelper {
     public static void showNotification(Context context, int notificationId, CharSequence tickerText, CharSequence contentText) {
 
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-
-        long when = System.currentTimeMillis();
         Notification notification = null;
 
         switch (notificationId) {
             case NOTIFICATION_PLAYING_ID:
-                notification = new Notification(R.drawable.ic_notification_playing, tickerText, when);
+                notification = new Notification.Builder(context)
+                        .setContentTitle(context.getString(R.string.app_name))
+                        .setContentText(contentText)
+                        .setTicker(tickerText)
+                        .setSmallIcon(R.drawable.ic_notification_playing)
+//                        .setLargeIcon(aBitmap)
+                        .build();
                 break;
             case NOTIFICATION_RECORDING_ID:
-                notification = new Notification(R.drawable.ic_notification_recording, tickerText, when);
+                notification = new Notification.Builder(context)
+                        .setContentTitle(context.getString(R.string.app_name))
+                        .setContentText(contentText)
+                        .setTicker(tickerText)
+                        .setSmallIcon(R.drawable.ic_notification_recording)
+//                        .setLargeIcon(aBitmap)
+                        .build();
                 break;
         }
 
-        CharSequence contentTitle = context.getString(R.string.app_name);
-        Intent notificationIntent = new Intent(context, MainActivity.class);
-        PendingIntent contentIntent = PendingIntent.getActivity(context, 0, notificationIntent, 0);
-
         if (notification != null) {
-            notification.setLatestEventInfo(context, contentTitle, contentText, contentIntent);
+            notification.contentIntent = PendingIntent.getActivity(context, 0, new Intent(context, MainActivity.class), 0);
             notification.flags = Notification.FLAG_ONGOING_EVENT;
+            notificationManager.notify(notificationId, notification);
         }
-
-        notificationManager.notify(notificationId, notification);
     }
 
     public static void cancelNotification(Context context, int notificationId) {
